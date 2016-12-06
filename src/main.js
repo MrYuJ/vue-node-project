b445cfdbcc503773541948e8c48155c5a2990a13
yujiayu <287356502@qq.com>
2016-12-06 17:09:44 +0800

    增加注释

--- src/main.js
+++ src/main.js
@@ -21,10 +21,20 @@ const routes = [
 const router = new VueRouter({routes});
 
 /*
-	扩展运算符（...）用于取出参数对象的所有可遍历属性，拷贝到当前对象之中。
+	1.扩展运算符（...）用于取出参数对象的所有可遍历属性，拷贝到当前对象之中。
 	let z = { a: 3, b: 4 };
 	let n = { ...z };
 	n // { a: 3, b: 4 }
+
+	2.ES6允许在对象之中，直接写变量。这时，属性名为变量名, 属性值为变量的值。
+	var birth = '2000/01/01';
+	var Person = {
+	  name: '张三',
+	  //等同于birth: birth
+	  birth,
+	  // 等同于hello: function ()...
+	  hello() { console.log('我的名字是', this.name); }
+	};
 */
 new Vue({
   el: '#app',
